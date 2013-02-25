9ba5d95804ca7ab68750e75fcd6b88c2621d94a7
Andrés Colón <levipr@gmail.com>
2013-02-24 21:10:44 -0400

    Updated jplayer filter html

--- lib/auto_html/filters/jplayer.rb
+++ lib/auto_html/filters/jplayer.rb
@@ -51,7 +51,7 @@ AutoHtml.add_filter(:jplayer).with({}) do |text, options|
 	jplayer_end, supplied = jplayer_end + "flv: '#{match}',", supplied + "flav," if text.include? "flv"		
 	jplayer_end, supplied = jplayer_end + "mp4: '#{match}',", supplied + "mp4," if text.include? "mp4"
 	jplayer_end, supplied = jplayer_end + "webm: '#{match}',", supplied + "webm," if text.include? "webm"			
-	jplayer_end += "			}).jPlayer("play");
+	jplayer_end += "			}).jPlayer('play');
 					        },
 					        swfPath: 'http://www.jplayer.org/2.1.0/js',
 					        supplied: '#{supplied}',
